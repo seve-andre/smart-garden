062869639fba38b06a52665273cdaad4c07acbaa
Andrea <andrea.severi12@studio.unibo.it>
2022-06-22 10:24:39 +0200

    add check light intensity task to scheduler

--- garden-controller/src/main.cpp
+++ garden-controller/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "tasks/Scheduler.h"
 #include "tasks/Task.h"
+#include "tasks/light/CheckLightIntensityTask.h"
 #include "tasks/temperature/CheckTemperatureTask.h"
 
 Scheduler scheduler;
@@ -11,7 +12,11 @@ void setup() {
     Task* checkTemperatureTask = new CheckTemperatureTask();
     checkTemperatureTask->init(40);
 
+    Task* checkLightIntensityTask = new CheckLightIntensityTask();
+    checkTemperatureTask->init(40);
+
     scheduler.addTask(checkTemperatureTask);
+    scheduler.addTask(checkLightIntensityTask);
 }
 
 void loop() {
