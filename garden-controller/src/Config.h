49ecf0f39a4f80cdef879797f49d120bd697612b
Andrea <andrea.severi12@studio.unibo.it>
2022-06-22 10:24:05 +0200

    add config file containing pins

---
+++ garden-controller/src/Config.h
@@ -0,0 +1,13 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define CONTROLLER_LED_1 3
+#define CONTROLLER_LED_2 4
+#define CONTROLLER_LED_3 5
+#define CONTROLLER_LED_4 6
+
+#define PHOTORESISTOR_PIN 0
+#define TEMP_SENSOR_PIN 1
+#define SERVO_PIN 9
+
+#endif
