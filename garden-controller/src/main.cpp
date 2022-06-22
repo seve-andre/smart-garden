a58c420e3d0866633b6c5aa8fe59835738bf911f
Andrea <andrea.severi12@studio.unibo.it>
2022-06-22 19:46:14 +0200

    add irrigation task to scheduler

--- garden-controller/src/main.cpp
+++ garden-controller/src/main.cpp
@@ -1,24 +1,45 @@
 #include <Arduino.h>
+#include "GardenState.h"
+#include "components/serial/MsgService.h"
 #include "tasks/Scheduler.h"
 #include "tasks/Task.h"
 #include "tasks/light/CheckLightIntensityTask.h"
+#include "tasks/servo/IrrigationTask.h"
 #include "tasks/temperature/CheckTemperatureTask.h"
 
 Scheduler scheduler;
 
 void setup() {
+    // initializes serial messagging service
+    MsgService.init();
+
     scheduler.init(10);
 
-    Task* checkTemperatureTask = new CheckTemperatureTask();
+    Task* irrigationTask = new IrrigationTask();
+    irrigationTask->init(40);
+    irrigationTask->setActive(false);
+
+    Task* checkTemperatureTask = new CheckTemperatureTask(irrigationTask);
     checkTemperatureTask->init(40);
 
-    Task* checkLightIntensityTask = new CheckLightIntensityTask();
+    Task* checkLightIntensityTask = new CheckLightIntensityTask(irrigationTask);
     checkTemperatureTask->init(40);
 
     scheduler.addTask(checkTemperatureTask);
     scheduler.addTask(checkLightIntensityTask);
+
+    // initially the state of the garden is set to AUTO
+    gardenState = AUTO;
 }
 
 void loop() {
+    /* switch (gardenState) {
+        case AUTO:
+            break;
+        case MANUAL:
+            break;
+        case ALARM:
+            break;
+    } */
     scheduler.schedule();
 }
