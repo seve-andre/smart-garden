b9c2c71f4a922da3ea7f3f9db5fef40ad250547c
Andrea <andrea.severi12@studio.unibo.it>
2022-07-11 09:34:37 +0200

    remove garden state and add bt msg service

--- garden-controller/src/main.cpp
+++ garden-controller/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 
 #include "Config.h"
-#include "GardenState.h"
 #include "components/bt/MsgServiceBT.h"
 #include "components/serial/MsgService.h"
 #include "tasks/Scheduler.h"
@@ -10,39 +9,28 @@
 #include "tasks/servo/IrrigationTask.h"
 
 Scheduler scheduler;
-// MsgServiceBT btMsgService(BT_RX_PIN, BT_TX_PIN);
-uint8_t gardenState;
+MsgServiceBT *btMsgService;
 
 void setup() {
     // initializes serial messagging service
     MsgService.init();
 
     // initializes bluetooth messagging service
-    // btMsgService.init();
+    btMsgService = new MsgServiceBT(BT_TXD_PIN, BT_RXD_PIN);
+    btMsgService->init();
 
     scheduler.init(10);
 
     Task *irrigationTask = new IrrigationTask();
     irrigationTask->init(40);
 
-    Task *lightSystemTask = new LightSystemTask((IrrigationTask *)irrigationTask);
+    Task *lightSystemTask = new LightSystemTask((IrrigationTask *)irrigationTask, btMsgService);
     lightSystemTask->init(40);
 
     scheduler.addTask(irrigationTask);
     scheduler.addTask(lightSystemTask);
-
-    // initially the state of the garden is set to AUTO
-    gardenState = AUTO;
 }
 
 void loop() {
-    switch (gardenState) {
-        case AUTO:
-            scheduler.schedule();
-            break;
-        case MANUAL:
-            break;
-        case ALARM:
-            break;
-    }
+    scheduler.schedule();
 }
